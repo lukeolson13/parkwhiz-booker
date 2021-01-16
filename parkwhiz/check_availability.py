163fc1b9c1a95010eb2d821533c7ac62810cf80f
Luke Olson <lukeolson13@gmail.com>
2021-01-15 19:35:09 -0700

    allow for different start times (if applicable)

--- parkwhiz/check_availability.py
+++ parkwhiz/check_availability.py
@@ -17,15 +17,37 @@ LOCATIONS = {
         'q': 'anchor_coordinates:39.50079,-106.154283 search_type:transient bounds:39.50829134860297,'
              '-106.16043976897441,39.50829134860297,-106.13579260107285,39.494053243956756,-106.13579260107285,'
              '39.494053243956756,-106.16043976897441',
-        'start_hour': 6,
-        'end_hour': 22,
     },
     'ELDORA': {
         'location_id': '37234',  # Main lot (free)
         'q': 'anchor_coordinates:39.937239,-105.582921 search_type:transient bounds:39.957239,'
-             '-105.602921,39.917238999999995,-105.562921 event_id:1075385',
-        'start_hour': 8,
-        'end_hour': 16,
+             '-105.602921,39.917238999999995,-105.562921',
+    }
+}
+TIMES = {
+    'COPPER': {
+        'ALL': {
+            'start_hour': 6,
+            'end_hour': 22,
+        },
+    },
+    'ELDORA': {
+        '9': {
+            'start_hour': 8,
+            'end_hour': 16,
+        },
+        '11': {
+            'start_hour': 10,
+            'end_hour': 16,
+        },
+        '1:30': {
+            'start_hour': 12.5,
+            'end_hour': 16,
+        },
+        '2:30': {
+            'start_hour': 13.5,
+            'end_hour': 16,
+        },
     }
 }
 
@@ -33,25 +55,44 @@ logger = logging.getLogger(__name__)
 
 
 class CheckAvailability:
-    def __init__(self, location, date, email, license_plate):
-        assert location.upper() in LOCATIONS.keys()
-        self.location = location
-        self.location_meta = LOCATIONS[self.location.upper()]
+    def __init__(self, location, date, email, license_plate, start_hour=None):
+        self.location = location.upper()
+        locations = list(LOCATIONS.keys())
+        if self.location not in locations:
+            raise ValueError(f'No handling for location "{self.location}". Must be one of: {locations}')
+
+        self.location_meta = LOCATIONS[self.location]
         self.date = date
         self.email = email
         self.license_plate = license_plate
+        self.start_hour = start_hour
+
+    def _get_start_and_end_times(self):
+        times = TIMES[self.location]
+        if len(times) == 1:  # only one option
+            vals = list(times.values())[0]
+        elif not self.start_hour:
+            raise ValueError(f'Must pass a start_hour. One of: {list(times.keys())}')
+        elif self.start_hour not in times.keys():
+            raise ValueError(f'"{self.start_hour}" not a valid start_hour. Must be one of: {list(times.keys())}')
+        else:
+            vals = times[self.start_hour]
+
+        return vals['start_hour'], vals['end_hour']
 
     @cached_property
     def get_params(self):
-        start_dt = (datetime.strptime(self.date, DATE_FORMAT)
-                    + timedelta(hours=self.location_meta['start_hour'])).strftime(DT_FORMAT)
-        end_dt = (datetime.strptime(self.date, DATE_FORMAT)
-                  + timedelta(hours=self.location_meta['end_hour'])).strftime(DT_FORMAT)
+        start_hour, end_hour = self._get_start_and_end_times()
+        start_dt = (datetime.strptime(self.date, DATE_FORMAT) + timedelta(hours=start_hour)).strftime(DT_FORMAT)
+        end_dt = (datetime.strptime(self.date, DATE_FORMAT) + timedelta(hours=end_hour)).strftime(DT_FORMAT)
         returns = 'curated'
         option_types = 'all'
         capabilities = 'capture_plate:always'
         fields = ('quote::default,quote:shuttle_times,location::default,location:timezone,location:site_url,location:'
                   'address2,location:description,location:msa,location:rating_summary')
+        q = self.location_meta['q']
+        if 'event_id' in q:
+            raise ValueError('"event_id" needs to be removed from the "q" param!')
         
         params = {
             'start_time': start_dt,
@@ -59,7 +100,7 @@ class CheckAvailability:
             'fields': fields,
             'option_types': option_types,
             'returns': returns,
-            'q': self.location_meta['q'],
+            'q': q,
             'capabilities': capabilities,
         }
 
@@ -116,9 +157,8 @@ class CheckAvailability:
                 cheapest_option = data['curated_data']['cheapest']['purchase_options'][0]
                 price = float(cheapest_option['price']['USD'])
                 if price != 0.0:
-                    raise ValueError("WTF! {location}'s lot ({location_id}) is no longer free. It's ${price}".format(
-                        location=self.location, location_id=self.location_meta['location_id'], price=price,
-                    ))
+                    raise ValueError(f"WTF! {self.location}'s lot ({self.location_meta['location_id']}) is no longer "
+                                     f"free. It's ${price}")
                 quote_id = cheapest_option['id']
                 success = self._book(quote_id)
                 fails += 1
